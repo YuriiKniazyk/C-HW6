7b8995302dbe8e7ea29122c929e7fcc59791e2f0
Yurii Kniazyk <y.kniazyk@gmail.com>
2020-07-26 00:43:49 +0300

    done

---
+++ HW6/HW6/PhoneBook.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HW6
+{
+    public class PhoneBook
+    {
+        private string readWriteWritePath = @"D:\Web\Training\SoftServe\HomeWork\HW6\HW6\phones.txt";
+        private string _newWritePath = @"D:\Web\Training\SoftServe\HomeWork\HW6\Phones.txt";
+        private string _newWritePathLastFile = @"D:\Web\Training\SoftServe\HomeWork\HW6\New.txt";
+
+        private readonly List<string> _phoneNumberFromFile = new List<string>();
+
+        public void WriteDataToFile(Dictionary<string, string> data)
+        {
+            using (StreamWriter sw = new StreamWriter(readWriteWritePath, false, System.Text.Encoding.Default))
+            {
+                foreach (var item in data)
+                {
+                    sw.WriteLine($"{item.Key}-{item.Value}");
+                }
+            }
+        }
+
+        public void ReadFile()
+        {
+            using (StreamReader sr = new StreamReader(readWriteWritePath, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var s = line.Split('-')[1];
+                    _phoneNumberFromFile.Add(s);
+                }
+            }
+        }
+
+        public void WritePhoneToNewFile()
+        {
+            using (StreamWriter sw = new StreamWriter(_newWritePath, false, System.Text.Encoding.Default))
+            {
+                foreach (var item in _phoneNumberFromFile)
+                {
+                    sw.WriteLine(item);
+                }
+            }
+        }
+
+        public string FindName(string name)
+        {
+            string answer = "";
+
+            using (StreamReader sr = new StreamReader(readWriteWritePath, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var s = line.Split('-')[0];
+                    if (s == name)
+                    {
+                        return answer = $"{line}";
+                    }
+                    else
+                    {
+                        return answer = $"Data not found!!";
+                    }
+                }
+            }
+
+            return answer;
+        }
+
+        public void ChangePhoneNumberWriteToNewFile()
+        {
+            using (StreamWriter sw = new StreamWriter(_newWritePathLastFile, false, System.Text.Encoding.Default))
+            {
+                foreach (var item in _phoneNumberFromFile)
+                {
+                    if (item[0] != '+')
+                    {
+                        var s = item;
+                        s = "+3" + item;
+                        sw.WriteLine(s);
+                    }
+                    else
+                    {
+                        sw.WriteLine(item);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HW6/HW6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>(9);
+            dictionary.Add("Yurii", "+380500110641");
+            dictionary.Add("Mama", "80500110642");
+            dictionary.Add("Tato", "80500110643");
+            dictionary.Add("Arsen", "+380500110644");
+            dictionary.Add("Sasha", "+380500110645");
+            dictionary.Add("Masha", "+380500110646");
+            dictionary.Add("Nina", "80500110647");
+            dictionary.Add("Dima", "+380500110648");
+            dictionary.Add("Katya", "+380500110649");
+
+            try
+            {
+                PhoneBook book = new PhoneBook();
+                book.WriteDataToFile(dictionary);
+                book.ReadFile();
+                book.WritePhoneToNewFile();
+
+                Console.WriteLine("Please enter name: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine(book.FindName(name));
+                book.ChangePhoneNumberWriteToNewFile();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
